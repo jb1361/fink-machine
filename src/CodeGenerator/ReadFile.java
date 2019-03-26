18161f5ddb6cbbcace4d84edf050930d3c07652e
jb1361 <justinbutler4@hotmail.com>
2019-03-26 17:33:11 -0400

    Add read data to array lists

--- src/CodeGenerator/ReadFile.java
+++ src/CodeGenerator/ReadFile.java
@@ -1,20 +1,61 @@
 package CodeGenerator;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class ReadFile {
-    public ReadFile(String fileName) {
+class ReadFile {
+    List<String> FsmSectionList = new ArrayList<String>();
+    List<String> CodeSectionList = new ArrayList<String>();
+
+    ReadFile(String fileName) {
         File file = new File(fileName);
         try {
             BufferedReader br = new BufferedReader(new FileReader(file));
             String line;
-            while ((line = br.readLine()) != null)
-                System.out.println(line);
+            line = br.readLine();
+            if(line != null) {
+                if (line.equals("FSM")) {
+                    ReadFSMSection(br);
+                } else if (line.equals("CODE")) {
+                    ReadCodeSection(br);
+                }
+            }
+            System.out.print(FsmSectionList);
+            System.out.print(CodeSectionList);
         }
         catch (Exception e) {
             e.printStackTrace();
         }
-
+    }
+    private void ReadFSMSection(BufferedReader br) {
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                if (line.equals("CODE")) {
+                    ReadCodeSection(br);
+                    return;
+                } else FsmSectionList.add(line);
+            }
+        }
+        catch(Exception e){
+                e.printStackTrace();
+            }
+    }
+    private void ReadCodeSection(BufferedReader br) {
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                if (line.equals("FSM")) {
+                    ReadFSMSection(br);
+                    return;
+                } else CodeSectionList.add(line);
+            }
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
 
